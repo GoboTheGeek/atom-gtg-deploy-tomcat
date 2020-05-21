a0c3015b0f4eee6023301e355c035974762d4af5
gob0 <gob0@opzo.ch>
2020-05-21 08:48:36 +0200

    First release

---
+++ README.md
@@ -0,0 +1,5 @@
+# gtg-deploy-tomcat package
+
+A plugin to deploy project to tomcat server (or any other)
+
+No screenshot available.
---
+++ spec/gtg-deploy-tomcat-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GtgDeployTomcatView from '../lib/gtg-deploy-tomcat-view';
+
+describe('GtgDeployTomcatView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
