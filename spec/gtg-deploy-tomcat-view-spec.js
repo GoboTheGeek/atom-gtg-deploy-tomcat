6e7a510d645b6e24a446a0b06113472a76be2d94
Gobo <65702336+GoboTheGeek@users.noreply.github.com>
2022-07-23 19:23:36 +0200

    Delete spec directory
    Give Up Github

--- spec/gtg-deploy-tomcat-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GtgDeployTomcatView from '../lib/gtg-deploy-tomcat-view';
-
-describe('GtgDeployTomcatView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
